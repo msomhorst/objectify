2db78802d9094f5563153644bae59af36c1159fa
Maarten Somhorst <msomhorst@bol.com>
2018-08-14 10:33:15 +0200

    Add test to show that an EmbeddedEntity is always indexed
    Change-Id: I94d9651030d657b340b32a343b5efc475cd928b8

--- src/test/java/com/googlecode/objectify/test/IndexingPartialTests.java
+++ src/test/java/com/googlecode/objectify/test/IndexingPartialTests.java
@@ -3,6 +3,9 @@
 
 package com.googlecode.objectify.test;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.Value;
+import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
@@ -17,6 +20,8 @@ import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 import static com.googlecode.objectify.ObjectifyService.factory;
 import static com.googlecode.objectify.ObjectifyService.ofy;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Tests of partial indexing.  Doesn't stress test the If mechanism; that is
@@ -93,5 +98,49 @@ class IndexingPartialTests extends TestBase {
 		thing.indexBar = false;
 		ofy().save().entity(thing).now();
 		assertThat(ofy().load().type(IndexedOnOtherField.class).filter("bar", true)).isEmpty();
+	}
+
+	/** */
+	@Entity
+	@Cache
+	@Data
+	private static class EmbeddedEntity {
+		@Id Long id;
+		boolean foo;
+	}
+
+	/** */
+	@Entity
+	@Cache
+	@Index
+	@Data
+	private static class UnindexEmbeddedEntity {
+		@Id Long id;
+		@Unindex EmbeddedEntity embeddedEntity;
+	}
+
+	/** */
+	@Test
+	void unindexEmbeddedEntity() throws Exception {
+		factory().register(EmbeddedEntity.class);
+		factory().register(UnindexEmbeddedEntity.class);
+
+		final EmbeddedEntity embeddedEntity = new EmbeddedEntity();
+		embeddedEntity.id = 1L;
+		embeddedEntity.foo = true;
+		final UnindexEmbeddedEntity thing = new UnindexEmbeddedEntity();
+		thing.id = 2L;
+		thing.embeddedEntity = embeddedEntity;
+
+		ofy().save().entity(thing).now();
+
+		Datastore datastore = ObjectifyService.factory().datastore();
+		com.google.cloud.datastore.Entity datastoreEntity = datastore.get(datastore.newKeyFactory().setKind("UnindexEmbeddedEntity").newKey(2L));
+		Value<?> datastoreEmbeddedEntity = datastoreEntity.getValue("embeddedEntity");
+		assertNotNull(datastoreEmbeddedEntity);
+		/**
+		 * The following line fails. Apparently, the embedded entity is indexed, dispite the @Unindex annotation
+		 */
+		assertTrue(datastoreEmbeddedEntity.excludeFromIndexes());
 	}
 }
